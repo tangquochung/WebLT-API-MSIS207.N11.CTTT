5fde64986809d6c03a6419fdc729889ed917aefd
tangquochung <19521583@gm.uit.edu.vn>
2022-10-25 15:28:22 +0700

    api up

--- src/index.js
+++ src/index.js
@@ -4,14 +4,27 @@ let app = express()
 
 let personRoute = require('./routes/person')
 
+let path = require('path')
 app.use((req, res, next) => {
-    console.log(`${new Date().toString()} => ${req.originalURl}`)
+    console.log(`${new Date().toString()} => ${req.originalUrl}`)
      //req.send(' ')
     next()
 })
-
+    
 app.use(personRoute)
 app.use(express.static('public'))
+
+//Handler for 404 Not Found - Resource not Found
+app.use((req, res, next) => {
+    res.status(404).send('We think you are lost!')
+})
+
+//Handler for Error 505
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+
+    res.sendFile(path.join(__dirname,'../public/500.html'))
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.info (`Server has started on ${PORT}`))
--- src/routes/person.js
+++ src/routes/person.js
@@ -18,5 +18,8 @@ router.get('/person/:name', (req, res) => {
     res.send(`You have request a person ${req.params.name}`)
 })
 
+router.get('/error', (req, res) => {
+    throw new Error('This is a forced error.')
+})
 
 module.exports = router 
