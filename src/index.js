7078d3ea98ee3bf341799c26784dce22d5af7d99
tangquochung <19521583@gm.uit.edu.vn>
2022-11-14 15:35:10 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 let express = require('express')
-
 let app = express()
-
 let personRoute = require('./routes/person')
-
+let customerRoute = require('./routes/customer')
 let path = require('path')
+let bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
 app.use((req, res, next) => {
-    console.log(`${new Date().toString()} => ${req.originalUrl}`)
+    console.log(`${new Date().toString()} => ${req.originalUrl}`, req.body)
      //req.send(' ')
     next()
 })
     
 app.use(personRoute)
+app.use(customerRoute)
 app.use(express.static('public'))
 
 //Handler for 404 Not Found - Resource not Found
